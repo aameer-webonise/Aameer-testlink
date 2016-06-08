4f692b806a7eca44ddcef6f580f24af5ba596efd
aameer-webonise <aameer.ausekar@gmail.com>
2016-06-08 16:08:47 +0530

    improve and refactor code to start automation of module

--- testlink/lib/automation/triggerAutomation.php
+++ testlink/lib/automation/triggerAutomation.php
@@ -37,51 +37,85 @@
 		//try{
 		$suite=$_POST['suiteName'];
 		$size=count($suite);
+		$j=0;
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_POST, 1);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
+		curl_setopt($curl, CURLOPT_URL,'http://10.0.0.100:9090/api/xml');
+		$result=curl_exec($curl);
+	    curl_close($curl);
+		//var_dump('hello '.$result[2]);
+		//print_r($result);
+		
+		
+		//exit();
+		
 		if($size>0){
 			for($i=0;$i<$size;$i++){
-				$curl = curl_init("http://10.0.0.100:9090/job/Proof_Automation/config.xml");
+				$j=$i+1;
+				$curl = curl_init();
 				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 				curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
-				$output = curl_exec($curl);
-				curl_close($curl);
 				
-				$suiteName=strtolower($suite[$i]);
-				$suiteName=str_replace(' ','', $suiteName);
-				$output=str_replace('pom.xml','pom_'.$suiteName.'.xml',$output);
-				
-				$xml = simplexml_load_string($output);
-				$xml->reporters->$node->recipients.=','.$user->emailAddress;
-				$output=$xml->asXML();
-				//var_dump($output);
-				$curl = curl_init();
-				curl_setopt($curl, CURLOPT_POST, 1);
-				curl_setopt($curl, CURLOPT_HTTPHEADER, Array("Content-Type: application/xml"));
-				curl_setopt($curl, CURLOPT_POSTFIELDS, $output);
-				curl_setopt($curl, CURLOPT_URL, 'http://10.0.0.100:9090/createItem?name='.$job_name.($i+1));
-				$result=curl_exec($curl);
-				curl_close($curl);
-				
-				$curl = curl_init();
-				curl_setopt($curl, CURLOPT_POST, 1);
-				curl_setopt($curl, CURLOPT_URL,'http://10.0.0.100:9090/job/'.$job_name.($i+1).'/buildWithParameters?token=trig');
-				$result=curl_exec($curl);
-				curl_close($curl);
+				for($j=$i+1;1;$j++)
+				{
+					curl_setopt($curl, CURLOPT_URL,'http://10.0.0.100:9090/api/xml');
+					$result=curl_exec($curl);
+					if (strpos($result, 'job/Testing'.$j) !== false) /*If job is present in testlink then check whether job is executing or not*/
+					{
+					    $curl=curl_init('http://10.0.0.100:9090/job/'.$job_name.$j.'/lastBuild/api/json?tree=result');
+						curl_setopt($curl, CURLOPT_POST, 1);
+						curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+						$result=curl_exec($curl);
+						if (strpos($result,'{"result":null}') !== false) /*if job is executing then go back to loop, else replace the config file and start this job*/
+						{
+							continue;
+						}
+						else 
+						{
+							$output=readJobConfig('Proof_Automation');
+							$output=replacePOM($suite[$i],$output,$node);
+							
+							$curl = curl_init();
+							curl_setopt($curl, CURLOPT_POST, 1);
+							curl_setopt($curl, CURLOPT_HTTPHEADER, Array("Content-Type: application/xml"));
+							curl_setopt($curl, CURLOPT_POSTFIELDS, $output);
+							curl_setopt($curl, CURLOPT_URL, 'http://10.0.0.100:9090/job/'.$job_name.$j.'/config.xml');
+							$result=curl_exec($curl);
+							
+							startBuild($job_name.$j);
+							
+							break;
+						}
+					    //continue;
+					}
+					else {
+						curl_close($curl);
+						$output=readJobConfig('Proof_Automation');
+						$output=replacePOM($suite[$i],$output,$node);
+						
+						$curl = curl_init();
+						curl_setopt($curl, CURLOPT_POST, 1);
+						curl_setopt($curl, CURLOPT_HTTPHEADER, Array("Content-Type: application/xml"));
+						curl_setopt($curl, CURLOPT_POSTFIELDS, $output);
+						curl_setopt($curl, CURLOPT_URL, 'http://10.0.0.100:9090/createItem?name='.$job_name.$j);
+						$result=curl_exec($curl);
+						curl_close($curl);
+						
+						startBuild($job_name.$j);
+						break;
+					}
+				}
 			}
 		}else{
-			$curl = curl_init();
-			curl_setopt($curl, CURLOPT_POST, 1);
-			curl_setopt($curl, CURLOPT_URL,'http://10.0.0.100:9090/job/Proof_Automation/buildWithParameters?token=trig');
-			$result=curl_exec($curl);
-			curl_close($curl);
-		}
-		//}catch(Exception $e){
+			$result=startBuild('Proof_Automation');
 			/*$curl = curl_init();
 			curl_setopt($curl, CURLOPT_POST, 1);
 			curl_setopt($curl, CURLOPT_URL,'http://10.0.0.100:9090/job/Proof_Automation/buildWithParameters?token=trig');
-			$result=curl_exec($curl);*/
-		//	echo 'error '.$e;
-		//	exit();
-		//}
+			$result=curl_exec($curl);
+			curl_close($curl);*/
+		}
 		?>
 		<div class="x-tl-panel-header x-unselectable" id="ext-gen3"><span class="x-tl-panel-header-text" id="ext-gen6">Automation</span></div>
 		<div class="x-tl-panel-bwrap" id="ext-gen4">
@@ -98,38 +132,6 @@
 		<h1>Having some problem, please try after some time</h1>
 		<?php
 		}
-		/*$demo = new java("Test");
-		$result=$demo->display();
-		echo result;
-		exit();*/
-		
-		//$demo = new java("javabridgedemo.JavaBridgeDemo");
-		
-		/*$test = new java("javabridgedemo.Test");
-		echo $test->display();*/
-		
-		/*$mail = new PHPMailer;
-		$mail->isSMTP();
-		$mail->SMTPSecure = 'ssl';
-		$mail->SMTPAuth = true;
-		$mail->Host = 'smtp.gmail.com';
-		$mail->Port = 465;
-		$mail->Username = 'email@weboapps.com';
-		$mail->Password = 'weboqa6186';
-		$mail->setFrom('aameer.ausekar@gmail.com');
-		$mail->addAddress('email@weboapps.com');
-		//$mail->addAddress('ausekar9@gmail.com');
-		
-		foreach($suite as $suiteName){
-			$mail->Subject = 'Trigger '.$projectName.' '.$suiteName;
-			$mail->Body = 'Trigger '.$projectName.' '.$suiteName;
-			if (!$mail->send()) {
-		  		echo "ERROR: " . $mail->ErrorInfo;
-				exit();
-			}	
-		}
-		echo 'Automation will start in 5 minutes';
-		*/
 		?>
 		
 		<?php
@@ -150,6 +152,35 @@
 		  $args->userID = isset($_SESSION['currentUser']) ? $_SESSION['currentUser']->dbID : 0;
 		        
 		  return $args;
+		}
+		
+		function startBuild($jobname){
+			$curl = curl_init();
+			curl_setopt($curl, CURLOPT_POST, 1);
+			curl_setopt($curl, CURLOPT_URL,'http://10.0.0.100:9090/job/'.$jobname.'/buildWithParameters?token=trig');
+			$result=curl_exec($curl);
+			curl_close($curl);
+			return $result;
+		}
+		
+		function readJobConfig($jobname){
+			$curl = curl_init("http://10.0.0.100:9090/job/Proof_Automation/config.xml");
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
+			$output = curl_exec($curl);
+			curl_close($curl);
+			return $output;
+		}
+		
+		function replacePOM($suiteName,$output,$node){
+			$suiteName=strtolower($suiteName);
+			$suiteName=str_replace(' ','', $suiteName);
+			$output=str_replace('pom.xml','pom_'.$suiteName.'.xml',$output);
+						
+			$xml = simplexml_load_string($output);
+			$xml->reporters->$node->recipients.=','.$user->emailAddress;
+			$output=$xml->asXML();
+			return $output;
 		}
 		?>
 	</body>
